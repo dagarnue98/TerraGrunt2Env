af0053ca96ed2c11659ca02484161d5986daa356
David Garcia Nuñez <dgarcnun@emeal.nttdata.com>
2022-03-29 10:08:39 +0000

    Added terragrunt-destroy.sh to /scripts

---
+++ scripts/terragrunt-destroy.sh
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# RUN TERRAGRUNT APPLY-ALL, OUTPUT THE PLAN TO THE TERMINAL AND TO A LOG FILE
+# ---------------------------------------------------------------------------------------------------------------------
+
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+
+# Run apply all and display output both to terminal and the log file temp.log
+terragrunt run-all destroy --terragrunt-working-dir --terragrunt-non-interactive 2>&1
+
+
