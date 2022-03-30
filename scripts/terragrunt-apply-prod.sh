6a6233d42ca432071a1b95968e31333f2e9fa26f
David Garcia Nuñez <dgarcnun@emeal.nttdata.com>
2022-03-30 11:32:56 +0200

    prueba

--- scripts/terragrunt-apply-prod.sh
+++ scripts/terragrunt-apply-prod.sh
@@ -5,9 +5,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
+LOG_FILE_NAME=${1:-plan.log}
 
 # Run apply all and display output both to terminal and the log file temp.log
-terragrunt run-all destroy  --terragrunt-non-interactive
+terragrunt apply-all --terragrunt-working-dir live/prod --terragrunt-non-interactive 2>&1 | tee temp.log
 
-
+# Remove bash colors from log file and put the output into the new log file
+sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" temp.log > ${LOG_FILE_NAME}
